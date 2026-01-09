cf40556d749986ef66c72af1093b665e661b9301
Mert Aydın <80180258+Merd0@users.noreply.github.com>
2026-01-09 17:04:40 +0300

    Update 08_ters_binary.c

--- 01_GPIO_Output/08_ters_binary.c
+++ 01_GPIO_Output/08_ters_binary.c
@@ -1,14 +1,14 @@
 /*
  * Dosya: 08_ters_binary.c
- * Açıklama: Binary sayıcının negatifi (Bitwise NOT ~ işlemi).
- * Yazar: Mert
+ * Açıklama: Negatif Binary Sayıcı.
+ * Mantık: Normal sayıcının tam tersidir.
+ * - Sayı 0: Hepsi YANAR (1111).
+ * - Sayı 1: Sadece 12 SÖNER (1110).
+ * - Sayı 15: Hepsi SÖNER (0000).
  */
 
-// while(1) dışındaki değişkenler:
 int sayac = 0;
-
-// while(1) içi:
-int ters_sayac = ~sayac; // Tüm bitleri tersle (0->1, 1->0)
+int ters_sayac = ~sayac; // Bitleri ters çevir (0->1, 1->0)
 
 for (int i = 0; i < 4; i++) {
     if ((ters_sayac >> i) & 1) 
